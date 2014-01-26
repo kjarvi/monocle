b432e2a6d3da239779cfffc2fac0ac1308dac984
Kurt Jarvi <20047m@gmail.com>
2014-01-25 18:00:42 -0600

    Made Monocle classless

--- monocle.lua
+++ monocle.lua
@@ -1,54 +1,52 @@
-local new = require 'monocle.lib.middleclass'
-Monocle = new('Monocle')
-
-function Monocle:initialize(initial)
-	self.active = initial.isActive or false
-	self.names = {}
-	self.listeners = {}
-	self.results = {}
-	self.text = ''
-	self.textCursorPosition = 0
-	self.printer = initial.customPrinter or false
-	self.printColor = initial.customColor or {128,128,128,128}
-	self.command = ''
-	self.debugToggle = initial.debugToggle or '`'
-	self.watchedFiles = initial.filesToWatch or {}
-	self.watchedFileTimes = {}
-	for i, v in ipairs(self.watchedFiles) do
+Monocle = {}
+function Monocle.new(initial)
+	Monocle.active = initial.isActive or false
+	Monocle.names = {}
+	Monocle.listeners = {}
+	Monocle.results = {}
+	Monocle.text = ''
+	Monocle.textCursorPosition = 0
+	Monocle.printer = initial.customPrinter or false
+	Monocle.printColor = initial.customColor or {128,128,128,128}
+	Monocle.command = ''
+	Monocle.debugToggle = initial.debugToggle or '`'
+	Monocle.watchedFiles = initial.filesToWatch or {}
+	Monocle.watchedFileTimes = {}
+	for i, v in ipairs(Monocle.watchedFiles) do
 		assert(love.filesystem.getLastModified(v),v .. ' must not exist D:')
-		self.watchedFileTimes[i] = love.filesystem.getLastModified(v)
+		Monocle.watchedFileTimes[i] = love.filesystem.getLastModified(v)
 	end
 
-	self:print('Monocle Initialized.')
+	--Monocle.print('Monocle Initialized.')
 end
 
-function Monocle:textinput(text)
-	if self.active and text ~= self.debugToggle then
-		--self.text = self.text .. text
-	elseif text == self.debugToggle then
-		self.active = not self.active
+function Monocle.textinput(text)
+	if Monocle.active and text ~= Monocle.debugToggle then
+		--Monocle.text = Monocle.text .. text
+	elseif text == Monocle.debugToggle then
+		Monocle.active = not Monocle.active
 	end
 end
 
-function Monocle:keypressed(key)
-	if self.active then
+function Monocle.keypressed(key)
+	if Monocle.active then
 		-- If entering a command:
 		if key == "return" then
 			-- parses string
 
-			self.results[self.text] = loadstring('return ' .. self.text)()
+			Monocle.results[Monocle.text] = loadstring('return ' .. Monocle.text)()
 
-			-- Clear self.text.
-			self.text = ''
+			-- Clear Monocle.text.
+			Monocle.text = ''
 
 		elseif key == 'backspace' then
-			self.text = string.sub(self.text,1,string.len(self.text)-1)
+			Monocle.text = string.sub(Monocle.text,1,string.len(Monocle.text)-1)
 		end
 	end
 end
 
-function Monocle:print(text,justtext)
-	if self.printer and not justtext then
+function Monocle.print(text,justtext)
+	if Monocle.printer and not justtext then
 		print("[Monocle]: " .. text)
 	elseif justtext then
 		return "[Monocle]: " .. text
@@ -56,50 +54,51 @@ function Monocle:print(text,justtext)
 end
 
 
-function Monocle:update()
-	for key,obj in ipairs(self.listeners) do
+function Monocle.update()
+	for key,obj in ipairs(Monocle.listeners) do
 		if type(obj) == 'string' then
-			self.results[key] = loadstring('return ' .. obj)() or 'Error!'
+			Monocle.results[key] = loadstring('return ' .. obj)() or 'Error!'
 		elseif type(obj) == 'table' then
-			self.results[key] = 'food'
+			Monocle.results[key] = 'food'
 		end
 	end
 
-	for i, v in ipairs(self.watchedFiles) do
-		if self.watchedFileTimes[i] ~= love.filesystem.getLastModified(v) then
+	for i, v in ipairs(Monocle.watchedFiles) do
+		if Monocle.watchedFileTimes[i] ~= love.filesystem.getLastModified(v) then
 			print('reloading')
-			self.watchedFileTimes[i] = love.filesystem.getLastModified(v)
+			Monocle.watchedFileTimes[i] = love.filesystem.getLastModified(v)
 			love.filesystem.load('main.lua')()
 		end
 	end
 end
 --blah
 
-function Monocle:watch(name,obj)
+function Monocle.watch(name,obj)
 	if type(obj) == 'string' then
-		self:print('Watching ' .. name)
-		table.insert(self.listeners,obj)
-		table.insert(self.names,name)
+		Monocle.print('Watching ' .. name)
+		table.insert(Monocle.listeners,obj)
+		table.insert(Monocle.names,name)
 	else
-		error(self:print('Object to watch is not a string'))
+		Monocle.print('Object to watch is not a string')
+		error('Object to watch is not a string')
 	end
 end
 --[[
-function Monocle:unwatch(name)
-	self.listeners[name] = nil
-	self.results = {}
+function Monocle.unwatch(name)
+	Monocle.listeners[name] = nil
+	Monocle.results = {}
 end
 --]]
-function Monocle:draw()
-	if self.active then
-		love.graphics.setColor(self.printColor)
-		--love.graphics.print("> " .. self.text .. "|", 0, 0)
+function Monocle.draw()
+	if Monocle.active then
+		love.graphics.setColor(Monocle.printColor)
+		--love.graphics.print("> " .. Monocle.text .. "|", 0, 0)
 		local draw_y = 0
-		for name,result in pairs(self.results) do
+		for name,result in pairs(Monocle.results) do
 			if type(result) == 'number' or type(result) == 'string' then
-				love.graphics.print(self.names[name] .. " : " .. result, 0, (draw_y + 1) * 15)
+				love.graphics.print(Monocle.names[name] .. " : " .. result, 0, (draw_y + 1) * 15)
 			elseif type(result) == 'table' then
-				love.graphics.print(self.names[name] .. " : Table:", 0, (draw_y + 1) * 15)
+				love.graphics.print(Monocle.names[name] .. " : Table:", 0, (draw_y + 1) * 15)
 				draw_y = draw_y + 1
 				for i,v in pairs(result) do
 					love.graphics.print("      " .. i .. " : " .. v, 0, (draw_y + 1) * 15)
@@ -108,6 +107,7 @@ function Monocle:draw()
 			end
 			draw_y = draw_y + 1
 		end
-	end -- self.active
+	end -- Monocle.active
 end
 
+return Monocle
